e5e9c08a04b3c3658dc62687ad9b37801b3b74d7
rof194 <carolinamargaritak@gmail.com>
2018-11-13 13:40:17 -0600

    added some stuff

--- cryptoSolver/src/application/Main.java
+++ cryptoSolver/src/application/Main.java
@@ -1,25 +1,54 @@
+/** Main.java **/
+
+/** Cryptogram Solver by 
+ * Zoey Abrigo & Carolina Kimbrough
+ */
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
 
 
 public class Main extends Application {
+	
+	public static Stage stage;
+	
+	/**  Loads the FXML file, sets the scene onto stage **/
 	@Override
 	public void start(Stage primaryStage) {
+		
+		stage = primaryStage; // Initializing the stage
+		
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			
+			// Load the FXML document
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation( getClass().getResource("/CryptoMain.fxml") );
+			
+			// Load the layout from the FXML and add it to the scene
+			AnchorPane layout = (AnchorPane) loader.load();
+			Scene scene = new Scene( layout );
+			
+			// Style sheet
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			
+			// Set the scene to stage and show the stage to user
+			stage.setScene( scene );
+			stage.setTitle( "Cryptogram Solver" );
+			//stage.getIcons().add(new Image("file:../../sudoku.png"));
+			
+			stage.show();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
+	/** Everything starts here **/
 	public static void main(String[] args) {
 		launch(args);
 	}
---
+++ cryptoSolver/src/application/controller/CryptoMainController.java
@@ -0,0 +1,22 @@
+/** MainController.java **/
+package application.controller;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+public class CryptoMainController implements EventHandler<ActionEvent> {
+
+
+	/**  This method loads automatically when program is run **/
+	public void initialize() {
+	
+	}
+
+	/** Method to handle ... **/
+	@Override
+	public void handle(ActionEvent event) {
+		
+	}
+	
+	
+}
